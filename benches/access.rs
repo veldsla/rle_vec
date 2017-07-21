71f6e64c8d109ee376d6991734d15e54b6b55de6
Kerollmops <renault.cle@gmail.com>
2017-07-21 14:28:03 +0200

    bench: Add tests for access functions

---
+++ benches/access.rs
@@ -0,0 +1,77 @@
+#![feature(test)]
+
+extern crate test;
+extern crate rle_vec;
+
+use std::iter::FromIterator;
+use std::iter::repeat;
+use test::Bencher;
+use rle_vec::RleVec;
+
+#[bench]
+fn rle_loop_10_000_unique_values(b: &mut Bencher) {
+    let rle = RleVec::from_iter(0..10_000);
+    b.iter(|| {
+        for (i, v) in rle.iter().enumerate() {
+            assert_eq!(i, *v);
+        }
+    })
+}
+
+#[bench]
+fn vec_loop_10_000_unique_values(b: &mut Bencher) {
+    let vec = Vec::from_iter(0..10_000);
+    b.iter(|| {
+        for (i, v) in vec.iter().enumerate() {
+            assert_eq!(i, *v);
+        }
+    })
+}
+
+#[bench]
+fn rle_loop_10_000_equal_values(b: &mut Bencher) {
+    let rle = RleVec::from_iter(repeat(0).take(10_000));
+    b.iter(|| {
+        for v in rle.iter() {
+            assert_eq!(*v, 0);
+        }
+    })
+}
+
+#[bench]
+fn vec_loop_10_000_equal_values(b: &mut Bencher) {
+    let vec = Vec::from_iter(repeat(0).take(10_000));
+    b.iter(|| {
+        for v in vec.iter() {
+            assert_eq!(*v, 0);
+        }
+    })
+}
+
+#[bench]
+fn rle_loop_10_000_runs_of_10_values(b: &mut Bencher) {
+    let zeros = repeat(0).take(10);
+    let ones = repeat(1).take(10);
+    let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
+
+    let rle = RleVec::from_iter(iter);
+    b.iter(|| {
+        for v in rle.iter() {
+            assert!(*v == 0 || *v == 1); // ugly
+        }
+    })
+}
+
+#[bench]
+fn vec_loop_10_000_runs_of_10_values(b: &mut Bencher) {
+    let zeros = repeat(0).take(10);
+    let ones = repeat(1).take(10);
+    let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
+
+    let vec = Vec::from_iter(iter);
+    b.iter(|| {
+        for v in vec.iter() {
+            assert!(*v == 0 || *v == 1); // ugly
+        }
+    })
+}
