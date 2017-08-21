ea16835e39d04f4c46160a2b18eb3a16f2686220
Arno Velds <a.velds@nki.nl>
2017-08-21 16:38:29 +0200

    test read with 2 buffersizes

--- benches/read.rs
+++ benches/read.rs
@@ -12,7 +12,7 @@ use std::io::{Cursor, Read};
 
 
 #[bench]
-fn rle_read_100000_u8(b: &mut Bencher) {
+fn rle_read_smallbuf_100000_u8(b: &mut Bencher) {
     let zeros = repeat(0u8).take(10);
     let ones = repeat(1u8).take(10);
     let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(100_000);
@@ -20,6 +20,27 @@ fn rle_read_100000_u8(b: &mut Bencher) {
     let rle = RleVec::from_iter(iter);
     
     let mut buf = vec![0; 1024];
+    b.iter(|| {
+        let mut rle = rle.clone();
+        loop {
+            match rle.read(&mut buf) {
+                Ok(n) if n == 0 => break,
+                Ok(_) => {},
+                Err(e) => panic!(e)
+            }
+        }
+    });
+}
+
+#[bench]
+fn rle_read_largebuf_100000_u8(b: &mut Bencher) {
+    let zeros = repeat(0u8).take(10);
+    let ones = repeat(1u8).take(10);
+    let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(100_000);
+
+    let rle = RleVec::from_iter(iter);
+
+    let mut buf = vec![0; 10000];
     b.iter(|| {
         let mut rle = rle.clone();
         loop {
