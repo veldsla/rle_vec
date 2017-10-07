4a4b18999da57c9080271592d9315015df301efe
Arno Velds <avelds@gmail.com>
2017-10-07 13:28:03 +0200

    More inserts per benchmark

--- benches/insert.rs
+++ benches/insert.rs
@@ -9,7 +9,7 @@ use test::Bencher;
 use rle_vec::RleVec;
 
 #[bench]
-fn rle_insert_middle_non_breaking_1000_runs_of_10_values(b: &mut Bencher) {
+fn rle_insert_runmids_non_breaking_1000_runs_of_10_values(b: &mut Bencher) {
     let zeros = repeat(0).take(10);
     let ones = repeat(1).take(10);
     let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
@@ -17,13 +17,20 @@ fn rle_insert_middle_non_breaking_1000_runs_of_10_values(b: &mut Bencher) {
 
     b.iter(|| {
         let mut rle = RleVec::from(&vec[..]);
-        let middle_value = rle[5_000];
-        rle.insert(5_000, middle_value);
+        let mut i = 5;
+        let mut val = 0;
+        for _ in 0..1000 {
+            rle.insert(i, val);
+            i += 11;
+            val = if val == 0 { 1 } else { 0 };
+        }
+        assert_eq!(rle.len(), 11000);
+        assert_eq!(rle.runs_len(), 1000);
     })
 }
 
 #[bench]
-fn rle_insert_middle_breaking_1000_runs_of_10_values(b: &mut Bencher) {
+fn rle_insert_runmids_breaking_1000_runs_of_10_values(b: &mut Bencher) {
     let zeros = repeat(0).take(10);
     let ones = repeat(1).take(10);
     let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
@@ -31,21 +38,34 @@ fn rle_insert_middle_breaking_1000_runs_of_10_values(b: &mut Bencher) {
 
     b.iter(|| {
         let mut rle = RleVec::from(&vec[..]);
-        rle.insert(5_000, 424242);
+        let mut i = 5;
+        let mut val = 1;
+        for _ in 0..1000 {
+            rle.insert(i, val);
+            i += 11;
+            val = if val == 0 { 1 } else { 0 };
+        }
+        assert_eq!(rle.len(), 11000);
+        assert_eq!(rle.runs_len(), 3000);
     })
 }
 
 #[bench]
-fn vec_insert_middle_1000_runs_of_10_values(b: &mut Bencher) {
+fn vec_insert_runmids_1000_runs_of_10_values(b: &mut Bencher) {
     let zeros = repeat(0).take(10);
     let ones = repeat(1).take(10);
     let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
-
-    let tmp_vec = Vec::from_iter(iter);
+    let vec = Vec::from_iter(iter);
 
     b.iter(|| {
-        let mut vec = tmp_vec.clone();
-        let middle_value = vec[5_000];
-        vec.insert(5_000, middle_value);
+        let mut vec = vec.clone();
+        let mut i = 5;
+        let mut val = 0;
+        for _ in 0..1000 {
+            vec.insert(i, val);
+            i += 11;
+            val = if val == 0 { 1 } else { 0 };
+        }
+        assert_eq!(vec.len(), 11000);
     })
 }
