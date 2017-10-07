04d30e491bbd966fbf8f6c21b18b52975ccac528
Arno Velds <avelds@gmail.com>
2017-10-07 13:54:36 +0200

    More removes per bench

--- benches/remove.rs
+++ benches/remove.rs
@@ -9,33 +9,80 @@ use test::Bencher;
 use rle_vec::RleVec;
 
 #[bench]
-fn rle_remove_middle_non_breaking_1000_runs_of_10_values(b: &mut Bencher) {
-    b.iter(|| {
-        let zeros = repeat(0).take(10);
-        let ones = repeat(1).take(10);
-        let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
+fn rle_remove_runmids_non_breaking_1000_runs_of_10_values(b: &mut Bencher) {
+    let zeros = repeat(0).take(10);
+    let ones = repeat(1).take(10);
+    let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
+    let vec: Vec<_> = iter.collect();
 
-        let mut rle = RleVec::from_iter(iter);
-        assert_eq!(rle.remove(5_000), 0);
+    b.iter(|| {
+        let mut rle = RleVec::from(&vec[..]);
+        let mut i = 5;
+        let mut val = 0;
+        for _ in 0..1000 {
+            assert_eq!(rle.remove(i), val);
+            i += 9;
+            val = if val == 0 { 1 } else { 0 };
+        }
+        assert_eq!(rle.len(), 9000);
+        assert_eq!(rle.runs_len(), 1000);
     })
 }
 
 #[bench]
-fn rle_remove_middle_breaking_1000_equal_values(b: &mut Bencher) {
+fn rle_remove_runs_merging_1000_runs(b: &mut Bencher) {
+    let zeros = repeat(0).take(10);
+    let ones = repeat(1).take(1);
+    let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(5_500);
+    let vec: Vec<_> = iter.collect();
+
     b.iter(|| {
-        let mut rle = RleVec::from_iter(0..10_000);
-        assert_eq!(rle.remove(5_000), 5_000);
+        let mut rle = RleVec::from(&vec[..]);
+        let mut i = 10;
+        for _ in 0..500 {
+            assert_eq!(rle.remove(i),1);
+            i += 10;
+        }
+        assert_eq!(rle.len(), 5_000);
+        assert_eq!(rle.runs_len(), 1);
     })
 }
 
 #[bench]
-fn vec_remove_middle_1000_runs_of_10_values(b: &mut Bencher) {
-    b.iter(|| {
-        let zeros = repeat(0).take(10);
-        let ones = repeat(1).take(10);
-        let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
+fn vec_remove_runmids_1000_runs_of_10_values(b: &mut Bencher) {
+    let zeros = repeat(0).take(10);
+    let ones = repeat(1).take(10);
+    let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(10_000);
+    let vec = Vec::from_iter(iter);
 
-        let mut vec = Vec::from_iter(iter);
-        assert_eq!(vec.remove(5_000), 0);
+    b.iter(|| {
+        let mut vec = vec.clone();
+        let mut i = 5;
+        let mut val = 0;
+        for _ in 0..1000 {
+            assert_eq!(vec.remove(i),val);
+            i += 9;
+            val = if val == 0 { 1 } else { 0 };
+        }
+        assert_eq!(vec.len(), 9000);
     })
 }
+
+#[bench]
+fn vec_remove_runs_merging_1000_runs(b: &mut Bencher) {
+    let zeros = repeat(0).take(10);
+    let ones = repeat(1).take(1);
+    let iter = repeat(zeros.chain(ones)).flat_map(|x| x).take(5_500);
+    let vec: Vec<_> = iter.collect();
+
+    b.iter(|| {
+        let mut vec = vec.clone();
+        let mut i = 10;
+        for _ in 0..500 {
+            assert_eq!(vec.remove(i),1);
+            i += 10;
+        }
+        assert_eq!(vec.len(), 5_000);
+    })
+}
+
